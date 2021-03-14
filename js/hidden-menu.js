dd268257189f2f512cb8b5599d4b7a4051c50c1e
Simon <simon.kotov.web@gmail.com>
2021-03-14 19:09:14 +0300

    Refactor the code. Optimized for mozilla v.2

--- js/hidden-menu.js
+++ js/hidden-menu.js
@@ -1,33 +1,40 @@
-const mailElement = document.querySelector(".header__mail__copy");
-const callBackElement = document.querySelector(".header-tel-callback");
-const categoryElement = document.querySelector(".drop-menu");
-const shadow = document.querySelector(".shadow");
+document.addEventListener("DOMContentLoaded", () => {
+    const mailElement = document.querySelector(".header__mail__copy");
+    const callBackElement = document.querySelector(".header-tel-callback");
+    const categoryElement = document.querySelector(".drop-menu");
+    const shadow = document.querySelector(".shadow");
+    const mobileMenuButton = document.querySelector(".mobile-search__button")
+    const emailButton = document.querySelector(".header__mail")
+    const phoneSection = document.querySelector(".tel-phone")
 
-// show the copy menu
-document.querySelector(".header__mail").addEventListener("click", e => {
-    e.stopPropagation(); 
-    mailElement.classList.toggle("header__mail__copy__hide");  
-});
+    // show the copy menu
+    emailButton.addEventListener("click", e => {
+        e.stopPropagation(); 
+        mailElement.classList.toggle("header__mail__copy__hide");  
+    });
 
-// remove the copy menu, by click anywhere
-document.addEventListener("click", () => mailElement.classList.add("header__mail__copy__hide"));
+    // remove the copy menu, by click anywhere
+    document.addEventListener("click", () => mailElement.classList.add("header__mail__copy__hide"));
 
-// show the callback menu, by hovering the phone number section
-document.querySelector(".tel-phone").addEventListener("mouseover", () =>  {
-    callBackElement.style.display = '';
-    shadow.style.display = '';
-    shadow.classList.add("shadow__block");
-    callBackElement.classList.remove("hide");
-});
+    // show the callback menu, by hovering the phone number section
+    phoneSection.addEventListener("mouseover", () =>  {
+        callBackElement.style.display = '';
+        shadow.style.display = '';
+        shadow.classList.add("shadow__block");
+        callBackElement.classList.remove("hide");
+    });
 
-// remove the callback menu, by mouseon shadow
-shadow.addEventListener("mouseover", () => {
-    shadow.classList.remove("shadow__block")
-    callBackElement.classList.add("hide")
-});
+    // remove the callback menu, by mouseon shadow
+    shadow.addEventListener("mouseover", () => {
+        shadow.classList.remove("shadow__block")
+        callBackElement.classList.add("hide")
+    });
 
-// add or remove the category menu at the mobile version of app by pressing a button
-document.querySelector(".mobile-search__button").addEventListener("click", () => categoryElement.classList.toggle("drop-menu__hide"))
+    // add or remove the category menu at the mobile version of app by pressing a button
+    mobileMenuButton.addEventListener("click", () => {
+        categoryElement.classList.toggle("drop-menu__hide")
+    })
+})
 
 
 
